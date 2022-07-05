ca113bdd454a612ccdff5d2a1c70b5f2c497ebb8
Sarah Ashcroft-Jones <49207455+sarahajones@users.noreply.github.com>
2022-07-05 15:54:50 +0100

    completion link

--- scripts/jspsych-lastScreen.js
+++ scripts/jspsych-lastScreen.js
@@ -82,7 +82,7 @@ jsPsych.plugins['jspsych-lastScreen'] = (function () {
 
         // define what happens when people click on the final submit button
         $('#tutorial-submit').on('click', function() {
-            location.href = "https://app.prolific.co/submissions/complete?cc=2D421A40";
+            location.href = "https://app.prolific.co/submissions/complete?cc=78C55EAD";
             if (document.exitFullscreen) {
                 document.exitFullscreen();
             } else if (document.msExitFullscreen) {
